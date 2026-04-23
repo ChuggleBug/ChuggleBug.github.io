6d68248a7f1fda4311a974824d68ab2ac292128b
Jacob <jacobgutierrez752@gmail.com>
2026-04-22 17:11:45 -0700

    Add next as root

---
+++ src/pages/app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  
+  return (
+    <div className="flex-1 flex flex-col items-center justify-center">
+        <h1>There is nothing here</h1>
+        <h1>please click this button</h1>
+
+        {/* 
+          Blog is not managed by Next.js. 
+          Would be unable to prefetch during development
+        */}
+        <a href="/blog/">Blog</a>
+
+    </div>
+  );
+}
