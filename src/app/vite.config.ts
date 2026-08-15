51a28231888b9d88f55b471661777b7a074b64eb
Jacob <jacobgutierrez752@gmail.com>
2026-08-15 14:35:04 -0700

    Remove vite ghpages 404 plugin

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 
@@ -9,7 +8,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    githubPagesSpa(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
