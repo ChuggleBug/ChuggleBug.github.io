199a4ff16ac1f0ba6dab563387b02393e58d27c1
Jacob <jacobgutierrez752@gmail.com>
2026-08-14 19:53:58 -0700

    Add types to fix build

--- src/app/src/components/ParticleBackground.tsx
+++ src/app/src/components/ParticleBackground.tsx
@@ -3,15 +3,16 @@
 import { ParticlesProvider, Particles } from "@tsparticles/react";
 import { useMemo, useCallback} from "react";
 import { loadSlim } from "@tsparticles/slim";
+import { type Engine, type Container } from "@tsparticles/engine";
 
 import particleOptions from "../utils/particleOptions"
 
-const particlesInit = async (engine) => {
+const particlesInit = async (engine: Engine) => {
   await loadSlim(engine);
 };
 
 export default function ParticleBackground() {
-    const particlesLoaded = useCallback(async (container) => {
+    const particlesLoaded = useCallback(async (container?: Container) => {
     console.log("Particles container loaded", container);
   }, []);
 
