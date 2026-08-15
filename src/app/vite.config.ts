40b8fbabccb6ee2a770343943394e698be1a5b29
Jacob <jacobgutierrez752@gmail.com>
2026-08-15 14:08:07 -0700

    Vite 404.redirect test 1

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    githubPagesSpa(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
