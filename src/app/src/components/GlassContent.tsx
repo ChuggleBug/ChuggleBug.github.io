ebaab1d47d10df3a2f9cd4bbdaa82392c74a3f00
Jacob <jacobgutierrez752@gmail.com>
2026-08-28 23:20:10 -0700

    Fix star button issues with div

--- src/app/src/components/GlassContent.tsx
+++ src/app/src/components/GlassContent.tsx
@@ -2,16 +2,14 @@
 import "../styles/glass-button.css"
 
 type GlassButtonProps = {
-  className?: string; 
+  className?: string;
   children: React.ReactNode;
 };
 
 export function GlassButton({ className = '', children }: GlassButtonProps) {
   return (
-    <button className={`glass-button ${className}`}>
-      <p className="text-center font-bold text-white w-full">
-        {children}
-      </p>
+    <button className={`glass-button ${className} w-full`}>
+      {children}
     </button>
   );
 }
