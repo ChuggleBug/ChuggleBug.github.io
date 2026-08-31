e1b5ba30258d73dc3b8ec0b5553897e736a88a0f
Jacob <jacobgutierrez752@gmail.com>
2026-08-31 16:23:17 -0700

    Implement toy page with initial toy

--- src/homepage/src/pages/Toys.tsx
+++ src/homepage/src/pages/Toys.tsx
@@ -1,8 +1,75 @@
+import "../styles/toy-element.css"
+
+import { useState } from "react";
+import TOYS, { type Toy } from "../utils/toy-data";
+import { Link } from "react-router";
+
+const thumbnails = import.meta.glob('../assets/toys/*/thumbnail.jpg', {
+    eager: true,
+    import: 'default',
+}) as Record<string, string>;
+
+const demos = import.meta.glob('../assets/toys/*/demo.gif', {
+    eager: true,
+    import: 'default',
+}) as Record<string, string>;
+
+function getThumbnail(title: string) {
+    return thumbnails[`../assets/toys/${title}/thumbnail.jpg`];
+}
+
+function getDemo(title: string) {
+    return demos[`../assets/toys/${title}/demo.gif`];
+}
+
+type ToyEmenentProps = {
+    toy: Toy;
+}
+
+function ToyElement({ toy }: ToyEmenentProps) {
+    // Source - https://stackoverflow.com/a/62785130
+    // Posted by Arthur Bruel
+    // Retrieved 2026-08-31, License - CC BY-SA 4.0
+    const [timeout, setModalTimeout] = useState<ReturnType<typeof setTimeout> | null>(null);
+    const [showDemo, setShowDemo] = useState<boolean>(false);
+
+    const handleMouseEnter = () => {
+        timeout && !showDemo && clearTimeout(timeout);
+        setModalTimeout(setTimeout(() => {
+            setShowDemo(true);
+        }, 500));
+
+    }
+
+    const handleMouseLeave = () => {
+        timeout && clearTimeout(timeout)
+        setShowDemo(false);
+    }
+
+    const children = (
+        <div className="glass-panel toy-element flex flex-col items-center-safe" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className="h-250 w-fit max-h-250 rounded flex justify-center pb-2">
+                {/* Show demo gif is there is one */}
+                <img className="overflow-clip rounded select-none"
+                    height={200}
+                    width={250}
+                    src={(showDemo && getDemo(toy.title) || getThumbnail(toy.title))}
+                    alt={`Thumbnail for the ${toy.title} toy`}
+                />
+
+            </div>
+            <p className="toy-title text-left w-full pb-1">{toy.title.replace('_', ' ')}</p>
+            <p>{toy.description}</p>
+        </div>
+    );
+
+    return (toy.useReactLink ?? false) ? <Link to={toy.webPath}>{children}</Link> : <a href={toy.webPath}>{children}</a>
+}
 
 export default function Toys() {
     return (
-        <div className="flex flex-col">
-            <div className="w-full lg:px-20 md:px-10 p-5">
+        <div className="flex flex-col gap-5 lg:px-20 md:px-10 px-5 pt-5">
+            <div className="w-full">
                 <div className="flex glass-panel p-10">
                     <div className="flex-2">
                         <h2>Toys That I've Made</h2>
@@ -13,6 +80,11 @@ export default function Toys() {
                         </p>
                     </div>
                 </div>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-between">
+                {TOYS.map((t, _) => {
+                    return <ToyElement key={t.title} toy={t} />;
+                })}
             </div>
         </div>
     );
---
+++ src/homepage/src/utils/toy-data.ts
@@ -0,0 +1,30 @@
+
+
+/*
+ * Structure for toys:
+ * title: Title of the toy card, with spaces replaced with "_"
+ *  - Note: A thumbnail.jpg and demo.gif should be provided under "assets/toys"
+ *  - Note: Both of these assets should be around 250px x 200px
+ * description: A short statement about what this "toy" is
+ * webPath: The path, relative to root
+ * useReactLink: use <Link> or <a>
+ *  - Note: In most cases <a> works fine. <Link> needs to be used if
+ *      webPath points to somewhere that is managed by the react homepage
+*/
+export type Toy = {
+    title: string,
+    description: string,
+    webPath: string,
+    useReactLink?: boolean
+};
+
+export const TOYS: ReadonlyArray<Toy> = [
+    {
+        title: "Star_Controls",
+        description: "I wanted to see how these \"stars\" would look like with different configurations, so I made a control panel.",
+        webPath: "/stars",
+        useReactLink: true,
+    },
+];
+
+export default TOYS;
