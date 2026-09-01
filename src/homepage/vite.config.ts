14c821c50d0c969cf401f6dfa2ac559be9a63d1a
Jacob <jacobgutierrez752@gmail.com>
2026-09-01 02:07:36 -0700

    Initial plan to restructure

--- src/homepage/vite.config.ts
+++ src/homepage/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
-import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,12 +11,4 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  build: {
-    rolldownOptions: {
-      input: {
-        main: resolve(import.meta.dirname, 'index.html'),
-        app1: resolve(import.meta.dirname, 'apps/test_app/index.html'),
-      }
-    }
-  }
 });
