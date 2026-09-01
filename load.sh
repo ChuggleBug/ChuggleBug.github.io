f11069cdb061c04fa843701131d310249a3416da
Jacob <jacobgutierrez752@gmail.com>
2026-09-01 13:50:47 -0700

    New workflow test 3

--- load.sh
+++ load.sh
@@ -10,8 +10,16 @@ mkdir -p _work
 
 
 load_repo() {
-    echo "Loading $2 to $1"
-    git clone "$2" "_work/$1"
+    local name="$1"
+    local repo_url="$2"
+    local url="$repo_url"
+
+    if [[ -n "$GH_LOAD_TOKEN" ]]; then
+        url="https://x-access-token:${GH_LOAD_TOKEN}@${repo_url#https://}"
+    fi
+
+    echo "Loading $name from $repo_url"
+    git clone "$url" "_work/$name"
 }
 
 load_repo "blog" https://github.com/ChuggleBug/Blog.git
