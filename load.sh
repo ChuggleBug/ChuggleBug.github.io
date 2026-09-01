d14aae1bac7cd47c4fcd4bc57bc7ae97bb31b7ba
Jacob <jacobgutierrez752@gmail.com>
2026-09-01 14:35:47 -0700

    New workflow test 5

--- load.sh
+++ load.sh
@@ -1,22 +1,23 @@
 #!/bin/bash 
 
-
-
 # Clean up work dir
 if [[ -d _work ]]; then
     rm -rf _work
 fi
 mkdir -p _work
 
-
+# Generic load utility
 load_repo() {
-    local name="$1"
-    local repo_url="$2"
-    local url="$repo_url"
-
-    echo "Loading $name from $repo_url"
-    git clone "$url" "_work/$name"
+    git clone "$2" "_work/$1"
 }
 
-load_repo "blog" https://github.com/ChuggleBug/Blog.git
+# Since blog is private, we need special permissions
+if [[ -n "${GH_BLOG_ACCESS_PAT}" ]]; then
+    blog_url="https://oauth2:${GH_BLOG_ACCESS_PAT}@github.com/ChuggleBug/Blog.git"
+else
+    blog_url=https://github.com/ChuggleBug/Blog.git
+fi
+load_repo "blog" "${blog_url}"
+
+# Apps
 load_repo "wikigraph" https://github.com/ChuggleBug/test_repo.git
